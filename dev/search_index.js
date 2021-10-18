38fbde34a9eb6640d763b05f9ce11b25f1533842
Documenter.jl <documenter@juliadocs.github.io>
2021-10-18 17:29:23 +0000

    build based on 2075295

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SMLMBaGoL","category":"page"},{"location":"#SMLMBaGoL","page":"Home","title":"SMLMBaGoL","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SMLMBaGoL.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SMLMBaGoL]","category":"page"}]
+}
