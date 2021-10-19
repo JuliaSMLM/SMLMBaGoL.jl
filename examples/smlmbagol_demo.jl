df665ca1032269973a6e0b5f20c35f34a314bc67
dschodt <35635770+dschodt@users.noreply.github.com>
2021-10-18 18:34:01 -0600

    Revise lambda estimate in constructpriors.jl

--- examples/smlmbagol_demo.jl
+++ examples/smlmbagol_demo.jl
@@ -11,12 +11,13 @@ using SpecialFunctions
 data = DataFrames.DataFrame(CSV.File(pwd() * "\\example_data.csv"))
 smld = SMLMData.SMLD(data)
 smld_preclustered = FrameConnection.precluster(smld)
-k, theta = SMLMBaGoL.constructprior_lambda(smld_preclustered)
+alpha, beta = SMLMBaGoL.constructprior_lambda(smld_preclustered, false)
+alpha = 1.0
 
-# clusterdata = FrameConnection.organizeclusters(smld_preclustered)
-# _, nobservations = FrameConnection.computeclusterinfo(clusterdata)
-# histogram(nobservations, normalize=:probability)
-# x = LinRange(1.0, 6.0, Int64(1e3))
-# gammadist(x) = (SpecialFunctions.gamma(k)*(theta^k))^(-1) *
-#     x.^(k-1.0) .* exp.(-x/theta)
-# plot!(x, gammadist(x))
+clusterdata = FrameConnection.organizeclusters(smld_preclustered)
+_, nobservations = FrameConnection.computeclusterinfo(clusterdata)
+histogram(nobservations, normalize=:probability)
+x = LinRange(1.0, 6.0, Int64(1e3))
+gammadist(x) = (beta^alpha / SpecialFunctions.gamma(alpha)) *
+    x.^(alpha-1.0) .* exp.(-beta*x)
+plot!(x, gammadist(x))
--- src/constructpriors.jl
+++ src/constructpriors.jl
@@ -1,10 +1,12 @@
 using FrameConnection
 using SMLMData
 using SpecialFunctions
+using Statistics
 using Optim
 
 """
-    k, θ = constructprior_lambda(smld_preclustered::SMLD)
+    α, β, clusterdata, nobservations = 
+        constructprior_lambda(smld_preclustered::SMLD, forceexp::Bool)
 
 Pre-cluster localizations in `smld` and define a Gamma prior from the results.
 
@@ -12,23 +14,52 @@ Pre-cluster localizations in `smld` and define a Gamma prior from the results.
 Localizations in the input structure `smld` are clustered together based on
 their spatiotemporal separations and then assumed to be a single emitter.  A
 Gamma prior is then constructed by fitting the distribution of localizations
-per emitter.
+per emitter.  The Gamma prior is defined using the convention with a shape 
+parameter `α` and a rate parameter `β`.  The flag `forcexp` forces the shape
+parameter `α` to be 1 (corresponding to the exponential distribution). 
 """
-function constructprior_lambda(smld_preclustered::SMLMData.SMLD)
+function constructprior_lambda(smld_preclustered::SMLMData.SMLD, forceexp::Bool=false)
     # Determine the number of localizations per pre-cluster.
     clusterdata = FrameConnection.organizeclusters(smld_preclustered)
     _, nobservations = FrameConnection.computeclusterinfo(clusterdata)
 
-    # Perform an MLE to estimate parameters for a Gamma prior.
-    nclusters = Float32(size(clusterdata)[1])
-    negloglikelihood(k) = 
-        -log((SpecialFunctions.gamma(k[1])*(k[2]^k[1]))^(-nclusters) *
-        prod(nobservations.^(k[1]-1.0) .* exp.(-nobservations/k[2])))
-    optimizer = Optim.NelderMead()
-    optimresults = Optim.optimize(negloglikelihood, [0, 0], [Inf, Inf], [1.0, 2.0], 
-        Fminbox(optimizer))
-    k = optimresults.minimizer[1]
-    theta = optimresults.minimizer[2]
+    # Compute the MLE for our prior.
+    if forceexp
+        alpha, beta = expMLE(clusterdata, nobservations)
+    else
+        alpha, beta = gammaMLE(clusterdata, nobservations)
+    end
 
-    return k, theta
+    return alpha, beta, clusterdata, nobservations
+end
+
+function gammaMLE(clusterdata, nobservations)
+    # Define initial guesses for our Gamma parameters.
+    # NOTE: These estimates were taken from the Gamma distribution wikipedia 
+    #       page https://en.wikipedia.org/wiki/Gamma_distribution
+    nclusters = Float32(size(clusterdata)[1])
+    s = log((1/nclusters)*sum(nobservations)) - 
+        (1/nclusters)*sum(log.(nobservations))
+    alphainit = 3.0 - s + sqrt((s-3.0)^2+24.0*s)/(12.0*s)
+    betainit = (1/(alphainit*nclusters)) * sum(nobservations)
+    initguess = [alphainit, betainit]
+
+    # Perform an MLE to estimate parameters for a Gamma prior.
+    negloglikelihood(k) = 
+        -log(((k[2]^k[1])/SpecialFunctions.gamma(k[1]))^(nclusters) *
+        prod(nobservations.^(k[1]-1.0) .* exp.(-k[2]*nobservations)))
+    optimizer = Optim.NelderMead()
+    optimresults = Optim.optimize(negloglikelihood, [0, 0], [Inf, Inf], initguess, 
+        Fminbox(optimizer))
+    alpha = optimresults.minimizer[1]
+    beta = optimresults.minimizer[2]
+
+    return alpha, beta
+end
+
+function expMLE(clusterdata, nobservations)
+    alpha = 1.0
+    beta = 1/Statistics.mean(nobservations)
+
+    return alpha, beta
 end
