742f4400358daabc3dce5e4fcb9a0d0cfa19a1dc
dschodt <35635770+dschodt@users.noreply.github.com>
2021-10-20 15:29:12 -0600

    Add inplace version of precluster_hierarchical()

--- src/precluster.jl
+++ src/precluster.jl
@@ -2,6 +2,27 @@ using SMLMData
 using Clustering
 
 """
+    precluster_hierarchical!(smld::SMLMData.SMLD2D, maxdist::Float64)
+
+Pre-cluster localizations in `smld` and define a Gamma prior from the results.
+
+# Description
+Localizations in the input structure `smld` are clustered using hierarchical
+using hierarchical clustering and stored in the output SMLD2D structure 
+`smld_preclustered`, with the field connectID updated to reflect cluster
+assignments.  The parameter `maxdist` defines the maximum distance allowed
+between emitters placed in the same precluster.
+"""
+function precluster_hierarchical!(smld::SMLMData.SMLD2D, maxdist::Float64=0.15)
+    # Compute the separations between all localizations in `smld`.
+    dist = SMLMBaGoL.pairwise_dist([smld.x smld.y])
+
+    # Perform hierarchical clustering based on `dist`.
+    htree = Clustering.hclust(dist; uplo=:U)
+    smld.connectID = Clustering.cutree(htree; h=maxdist)
+end
+
+"""
     smld_preclustered = precluster_hierarchical(smld::SMLMData.SMLD2D,
                                                 maxdist::Float64)
 
@@ -26,6 +47,15 @@ function precluster_hierarchical(smld::SMLMData.SMLD2D, maxdist::Float64=0.15)
     return smld_preclustered
 end
 
+"""
+dist = pairwise_dist(data::Matrix{Float64})
+
+Compute the pairwise distance between rows of data.
+
+# Description
+This method computes the Euclidean distance between pairs of data organized as
+rows of the input matrix `data`.
+"""
 function pairwise_dist(data::Matrix{Float64})
     # Compute the distance between rows of `data`.
     npoints = size(data)[1]
