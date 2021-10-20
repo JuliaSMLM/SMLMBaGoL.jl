eb180759b3d8053a4d0046254b3d45d46d027695
dschodt <35635770+dschodt@users.noreply.github.com>
2021-10-20 13:05:12 -0600

    Add initial version of hierarchical clustering

--- examples/smlmbagol_demo.jl
+++ examples/smlmbagol_demo.jl
@@ -18,6 +18,13 @@ roisize = 5
 roioverlap = 1
 smld_subregions, rois = SMLMBaGoL.gensubregions(smld, roisize, roioverlap)
 
+## Perform hierarchical clustering.
+maxdist = 0.15 # pixels
+smld_preclustered = SMLMBaGoL.precluster_hierarchical.(smld_subregions, maxdist)
+smld_test = smld_preclustered[1]
+smld_test1 = SMLMData.isolatedata(smld_test, smld_test.connectID .== 1)
+dist1 = SMLMBaGoL.pairwise_dist([smld_test1.x smld_test1.y])
+
 # smld_preclustered = FrameConnection.precluster(smld) # not meaningful, just to test!
 # alpha, beta = SMLMBaGoL.constructprior_lambda(smld_preclustered, false)
 
