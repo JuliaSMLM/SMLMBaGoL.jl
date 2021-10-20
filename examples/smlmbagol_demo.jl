835191faa8eccfb71c241650c07b52082afabc97
dschodt <35635770+dschodt@users.noreply.github.com>
2021-10-20 13:14:46 -0600

    Update gensubregions() to account for method name change

--- examples/smlmbagol_demo.jl
+++ examples/smlmbagol_demo.jl
@@ -22,7 +22,7 @@ smld_subregions, rois = SMLMBaGoL.gensubregions(smld, roisize, roioverlap)
 maxdist = 0.15 # pixels
 smld_preclustered = SMLMBaGoL.precluster_hierarchical.(smld_subregions, maxdist)
 smld_test = smld_preclustered[1]
-smld_test1 = SMLMData.isolatedata(smld_test, smld_test.connectID .== 1)
+smld_test1 = SMLMData.isolatesmld(smld_test, smld_test.connectID .== 1)
 dist1 = SMLMBaGoL.pairwise_dist([smld_test1.x smld_test1.y])
 
 # smld_preclustered = FrameConnection.precluster(smld) # not meaningful, just to test!
