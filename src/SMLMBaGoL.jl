b38d81d7e111f02d095688510ec81a567dc8f25e
dschodt <35635770+dschodt@users.noreply.github.com>
2021-10-25 18:58:29 -0600

    Define jump proposal distribution in probjump.jll

--- src/SMLMBaGoL.jl
+++ src/SMLMBaGoL.jl
@@ -8,5 +8,10 @@ include("priordrift.jl")
 include("gensubregions.jl")
 include("removeoutliers.jl")
 include("precluster.jl")
+include("probjump.jl")
+include("allocatelocs.jl")
+include("moveemitters.jl")
+include("runRJMCMC.jl")
+include("runbagol.jl")
 
 end
