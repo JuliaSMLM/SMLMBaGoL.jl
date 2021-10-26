8a94a9a8c0fcf79ed6a1175b1607b7a064f63e9c
dschodt <35635770+dschodt@users.noreply.github.com>
2021-10-25 18:58:29 -0600

    Define jump proposal distribution in probjump.jll

---
+++ src/probjump.jl
@@ -0,0 +1,16 @@
+using Distributions
+
+"""
+    distribution = jumpdistrib(jumppmf::Vector{Float64})
+
+Generate a distribution of jump choices from the provided `jumppmf`.
+
+# Description
+This function generates a distribution of the jump probabilities that can be 
+sampled to propose a jump labeled by an index in the range [1, length(jumppmf)]
+using rand(distribution).
+"""
+function jumpdistrib(jumppmf::Vector{Float64})
+    # Create a distribution for the jumps using the Distributions package.
+    return Distributions.DiscreteNonParametric(1:length(jumppmf), jumppmf)
+end
