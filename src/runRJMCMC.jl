dd5a8bebd6402b57f9396be8663165cd3a1c0009
dschodt <35635770+dschodt@users.noreply.github.com>
2021-11-02 15:54:11 -0600

    Revise chain building workflow

--- src/runRJMCMC.jl
+++ src/runRJMCMC.jl
@@ -7,7 +7,7 @@ function runRJMCMC!(smld::Matrix{SMLMData.SMLD2D},
                     mcparams::MCParams2D)
     # Loop over subregions in `smld` and perform RJMCMC.
     smldsize = size(smld)
-    chain = Matrix{Vector{SMLMBaGoL.BaGoLChain}}(undef, smldsize)
+    chain = Matrix{Vector{SMLMBaGoL.BaGoLChain2D}}(undef, smldsize)
     for ii = 1:smldsize[1], jj = 1:smldsize[2]
         # Generate a distinct SMLD2D for each precluster.
         smldclusters, _ = SMLMData.isolateconnected(smld[ii, jj])
@@ -24,7 +24,7 @@ function runRJMCMC!(smld::Vector{SMLMData.SMLD2D},
                     mcparams::MCParams2D)
     # Loop over preclusters and perform RJMCMC on each of them.
     nclusters = Base.length(smld)
-    chain = Vector{SMLMBaGoL.BaGoLChain}(undef, nclusters)
+    chain = Vector{SMLMBaGoL.BaGoLChain2D}(undef, nclusters)
     for nn = 1:nclusters
         chain[nn] = runRJMCMC!(smld[nn], mcparams)
     end
@@ -56,136 +56,193 @@ function runRJMCMC!(smld::SMLMData.SMLD2D,
     z = SMLMBaGoL.allocatelocs(smld, μ, a)
 
     # Run the chain for the burn-in iterations.
-    initchain = SMLMBaGoL.BaGoLChain([k], [μ], [a], [z])
-    SMLMBaGoL.burnin!(smld, initchain, mcparams)
+    initstate = SMLMBaGoL.BaGoLState2D(k, z, μ, a)
+    initchain = SMLMBaGoL.buildchain(smld, initstate, mcparams, true)
     
     # Run the chain for the remaining true iterations.
-    chain = SMLMBaGoL.buildchain(smld, SMLMBaGoL.getstate(initchain), mcparams)
+    chain = SMLMBaGoL.buildchain(smld, initchain, mcparams)
 
     return chain
 end
 
-function burnin!(smld::SMLMData.SMLD2D, 
-                 initchain::SMLMBaGoL.BaGoLChain,
-                 mcparams::SMLMBaGoL.MCParams2D)
-    # Run the chain for the burn-in iterations.
-    for ii = 1:mcparams.n_burnin
+function buildchain(smld::SMLMData.SMLD2D, 
+                    initstate::SMLMBaGoL.BaGoLState2D,
+                    mcparams::SMLMBaGoL.MCParams2D,
+                    burnin::Bool = false)
+    # Run the chain for the number of iterations specified in `mcparams`. If
+    # this is a burn-in run (`burnin=true`), we'll only keep the most recent
+    # state after each iteration.
+    niter = burnin ? mcparams.n_burnin : mcparams.n_chain
+    state = deepcopy(initstate)
+    accepted = true
+    chain = SMLMBaGoL.BaGoLChain2D([state], 1, [1; 2; 3; 4], [accepted])
+    for ii = 1:niter
         # If there is only one emitter, allocate all localizations to that 
         # emitter.
-        if initchain.k[end] == 1
-            initchain.z[end] = ones(Int, Base.length(initchain.z[end]))
+        if state.k == 1
+            state.z = ones(Int, Base.length(state.z))
         end
 
         # If any emitters in the chain have no allocations, remove them before
         # proceeding.
-        for kk in initchain.k[end]:-1:1
-            if !any(initchain.z[end] .== kk)
-                SMLMBaGoL.removeemitter!(initchain, kk)
+        for kk in state.k:-1:1
+            if !any(state.z .== kk)
+                SMLMBaGoL.removeemitter!(state, kk)
             end
         end
 
         # Select a jump.
         jumptype = Distributions.rand(mcparams.jumpdistrib)
 
-        # Update the chain based on the jump.
-        SMLMBaGoL.updatechain!(smld, initchain, mcparams, jumptype)
+        # Update the state based on the proposed jump.
+        state, accepted = SMLMBaGoL.updatestate(smld, state, mcparams, jumptype)
+
+        # If needed, store this state in the output chain.
+        if !burnin
+            SMLMBaGoL.addstate!(chain, state, accepted)
+        end
+    end
+
+    # Determine what to return based on `burnin`.
+    if burnin
+        return SMLMBaGoL.BaGoLChain2D([state], 1, [1; 2; 3; 4], [accepted])
+    else
+        return chain
     end
 end
 
 function buildchain(smld::SMLMData.SMLD2D, 
-                    chain::SMLMBaGoL.BaGoLChain,
-                    mcparams::SMLMBaGoL.MCParams2D)
-    # Run the chain for the true (not burn-in) iterations.
-    for ii = 1:mcparams.n_chain
-        # If there is only one emitter, allocate all localizations to that 
-        # emitter.
-        if chain.k[end] == 1
-            chain.z[end] = ones(Int, Base.length(chain.z[end]))
-        end
-
-        # If any emitters in the chain have no allocations, remove them before
-        # proceeding.
-        for kk in chain.k[end]:-1:1
-            if !any(chain.z[end] .== kk)
-                SMLMBaGoL.removeemitter!(chain, kk)
-            end
-        end
-
-        # Select a jump.
-        jumptype = Distributions.rand(mcparams.jumpdistrib)
-
-        # Update the chain based on the jump.
-        SMLMBaGoL.updatechain!(smld, chain, mcparams, jumptype)
-    end
-
-    return chain
+                    initchain::SMLMBaGoL.BaGoLChain2D,
+                    mcparams::SMLMBaGoL.MCParams2D,
+                    burnin::Bool = false)
+    # Call the version of buildchain() with a state input.
+    return buildchain(smld, initchain.states[end], mcparams, burnin)
 end
 
-function updatechain!(smld::SMLMData.SMLD2D, 
-                      chain::SMLMBaGoL.BaGoLChain,
-                      mcparams::SMLMBaGoL.MCParams2D, 
-                      jumptype::Int)  
-    # Update the chain based on the specified jump.
+function updatestate(smld::SMLMData.SMLD2D, 
+                     state::SMLMBaGoL.BaGoLState2D,
+                     mcparams::SMLMBaGoL.MCParams2D, 
+                     jumptype::Int)  
+    # Update the state based on the specified jump.
     if jumptype == 1
         # Jump type 1 is a move of the existing emitters.
         # Move jumps are done by Gibbs sampling so are always accepted. 
-        proposal = SMLMBaGoL.proposemove(smld, chain, mcparams)
-        acceptance = 1.0
+        state, accepted = SMLMBaGoL.move(smld, state, mcparams)
     elseif jumptype == 2
         # Jump type 2 is a reallocation of localizations to emitters.
         # Reallocation jumps are done by Gibbs sampling so are always accepted.
-        proposal = SMLMBaGoL.proposeallocation(smld, chain)
-        acceptance = 1.0
+        state, accepted = SMLMBaGoL.reallocate(smld, state)
     elseif jumptype == 3
         # Jump type 3 is a birth of new emitter.  If there are already 
         # as many emitters as localizations, we'll just return the current
-        # state of the chain.
-        if chain.k[end] < SMLMData.length(smld)
-            proposal, positionind = SMLMBaGoL.proposebirth(smld, chain, mcparams)
-            acceptance = SMLMBaGoL.acceptbirth(smld, 
-                                               proposal, 
-                                               positionind, 
-                                               chain, 
-                                               mcparams)
-        else
-            proposal = getstate(chain)
-            acceptance = 1.0
-        end
+        # state.
+        state, accepted = SMLMBaGoL.birth(smld, state, mcparams)
     elseif jumptype == 4
         # Jump type 4 is a death of an existing emitter.  If there is only 1
         # emitter, we don't want to remove it so we'll return the current
-        # state of the chain.
-        if chain.k[end] == 1
-            proposal = getstate(chain)
-            acceptance = 1.0
-        else
-            proposal, positionind = SMLMBaGoL.proposedeath(smld, chain)
-            acceptance = 1 / SMLMBaGoL.acceptdeath(smld, 
-                                                   proposal, 
-                                                   positionind, 
-                                                   chain, 
-                                                   mcparams)
-        end
+        # state.
+        state, accepted = SMLMBaGoL.death(smld, state, mcparams)
     else
         error("Unknown jump type!")
     end
 
-    # Test the jump and store it if it's accepted.
-    if Base.rand() <= acceptance
-        SMLMBaGoL.catchain!(chain, proposal)
+    return state, accepted
+end
+
+function move(smld::SMLMData.SMLD2D, 
+              state::SMLMBaGoL.BaGoLState2D,
+              mcparams::SMLMBaGoL.MCParams2D)
+    # Propose a move and accept it (emitter moves are always accepted).
+    return SMLMBaGoL.proposemove(smld, state, mcparams), true
+end
+
+function reallocate(smld::SMLMData.SMLD2D, 
+                    state::SMLMBaGoL.BaGoLState2D)
+    # Propose an allocation and accept it (allocations are always accepted).
+    return SMLMBaGoL.proposeallocation(smld, state), true
+end
+
+function birth(smld::SMLMData.SMLD2D, 
+               state::SMLMBaGoL.BaGoLState2D,
+               mcparams::SMLMBaGoL.MCParams2D)
+    # Propose a birth of a new emitter (unless there are as many emitters as
+    # localizations, in which case we'll return the input `state`).
+    if state.k < SMLMData.length(smld)
+        proposal, positionind = SMLMBaGoL.proposebirth(smld, state, mcparams)
+        acceptance = SMLMBaGoL.acceptbirth(smld, 
+                                           proposal, 
+                                           positionind, 
+                                           state, 
+                                           mcparams)
     else
-        SMLMBaGoL.keepstate!(chain)
+        proposal = deepcopy(state)
+        acceptance = 1.0
+    end
+
+    # Determine whether or not we should accept the new emitter.
+    if Base.rand() <= acceptance
+        return proposal, true
+    else
+        return state, false
     end
 end
 
-function catchain!(chain1::SMLMBaGoL.BaGoLChain, chain2::SMLMBaGoL.BaGoLChain)
-    fields = fieldnames(SMLMBaGoL.BaGoLChain)
+function death(smld::SMLMData.SMLD2D, 
+               state::SMLMBaGoL.BaGoLState2D,
+               mcparams::SMLMBaGoL.MCParams2D)
+    # Propose the death of a random emitter (unless there is only 1 emitter 
+    # left, in which case we should return the current state).
+        if state.k == 1
+            proposal = deepcopy(state)
+            acceptance = 1.0
+        else
+            proposal, positionind = SMLMBaGoL.proposedeath(smld, state)
+            acceptance = SMLMBaGoL.acceptdeath(smld, 
+                                               proposal, 
+                                               positionind, 
+                                               state, 
+                                               mcparams)
+        end
+
+    # Determine whether or not we should accept emitter death.
+    if Base.rand() <= acceptance
+        return proposal, true
+    else
+        return state, false
+    end
+end
+
+function addstate!(chain::SMLMBaGoL.BaGoLChain2D, 
+                   state::SMLMBaGoL.BaGoLState2D, 
+                   accepted::Bool)
+    # Update the chain to include `state`.
+    push!(chain.states, state)
+    push!(chain.accept, accepted)
+    chain.n += 1
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function catchain!(chain1::SMLMBaGoL.BaGoLChain2D, chain2::SMLMBaGoL.BaGoLChain2D)
+    fields = fieldnames(SMLMBaGoL.BaGoLChain2D)
     for ff in fields
         setfield!(chain1, ff, [getfield(chain1, ff); getfield(chain2, ff)])
     end
 end
-function catchain(chain1::SMLMBaGoL.BaGoLChain, chain2::SMLMBaGoL.BaGoLChain)
-    fields = fieldnames(SMLMBaGoL.BaGoLChain)
+function catchain(chain1::SMLMBaGoL.BaGoLChain2D, chain2::SMLMBaGoL.BaGoLChain2D)
+    fields = fieldnames(SMLMBaGoL.BaGoLChain2D)
     chain = SMLMBaGoL.BaGoLChain(SMLMBaGoL.length(chain1) 
         + SMLMBaGoL.length(chain2))
     for ff in fields
@@ -195,19 +252,19 @@ function catchain(chain1::SMLMBaGoL.BaGoLChain, chain2::SMLMBaGoL.BaGoLChain)
     return chain
 end
 
-function keepstate!(chain::SMLMBaGoL.BaGoLChain)
-    fields = fieldnames(SMLMBaGoL.BaGoLChain)
+function keepstate!(chain::SMLMBaGoL.BaGoLChain2D)
+    fields = fieldnames(SMLMBaGoL.BaGoLChain2D)
     for ff in fields
         setfield!(chain, ff, [getfield(chain, ff); [getfield(chain, ff)[end]]])
     end
 end
 
-function getstate(chain::SMLMBaGoL.BaGoLChain)
-    return SMLMBaGoL.BaGoLChain(chain.k[end], chain.μ[end], chain.a[end], chain.z[end])
+function getstate(chain::SMLMBaGoL.BaGoLChain2D)
+    return SMLMBaGoL.BaGoLChain2D(chain.k[end], chain.μ[end], chain.a[end], chain.z[end])
 end
-function getstate(chain::SMLMBaGoL.BaGoLChain, ind::Vector{Int})
-    return SMLMBaGoL.BaGoLChain(chain.k[ind], chain.μ[ind], chain.a[ind], chain.z[ind])
+function getstate(chain::SMLMBaGoL.BaGoLChain2D, ind::Vector{Int})
+    return SMLMBaGoL.BaGoLChain2D(chain.k[ind], chain.μ[ind], chain.a[ind], chain.z[ind])
 end
-function getstate(chain::SMLMBaGoL.BaGoLChain, ind::Int)
-    return SMLMBaGoL.BaGoLChain(chain.k[ind], chain.μ[ind], chain.a[ind], chain.z[ind])
+function getstate(chain::SMLMBaGoL.BaGoLChain2D, ind::Int)
+    return SMLMBaGoL.BaGoLChain2D(chain.k[ind], chain.μ[ind], chain.a[ind], chain.z[ind])
 end
