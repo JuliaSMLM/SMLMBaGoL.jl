f5558073e8f811afce4aba860e168eb7f760d0f8
dschodt <35635770+dschodt@users.noreply.github.com>
2022-05-03 12:48:52 -0600

    Update top level interface in package

--- examples/smlmbagol_demo.jl
+++ examples/smlmbagol_demo.jl
@@ -3,35 +3,22 @@ using Revise
 using SMLMBaGoL
 using SMLMData
 using SMLMSim
-using DataFrames
-using CSV
 using Plots
-using SpecialFunctions
-using StatsBase
 using ImageView
+using StatsBase
 
 # Simulate some data.
-γ = 1e5 # Fluorophore emission rate
-q = [0 1
-    1e-3 0] # Fluorophore blinking rates
-n = 6 # Nmer rank
-d = 0.5 # Nmer diameter
-ρ = 0.1 # density of Nmers 
-xsize = 32.0 # image size
-ysize = 32.0
-nframes = 10000 # number of frames
-ndatasets = 1
-framerate = 1.0 # set to 1.0 to keep things in physical units
-σ_psf = 1.3 # psf sigma used for uncertainty calcs
-minphotons = 500 # minimum number of photons per frame accepted
-f = SMLMSim.GenericFluor(γ, q)
-pattern = SMLMSim.Nmer2D(n, d)
-smld_true = SMLMSim.uniform2D(ρ, pattern, xsize, ysize)
-smld_true.datasize = [ysize; xsize]
-smld_model = SMLMSim.kineticmodel(smld_true, f, nframes, framerate; ndatasets = ndatasets, minphotons = minphotons)
-smld_model.datasize = [ysize; xsize]
-smld = SMLMSim.noise(smld_model, σ_psf)
-smld.datasize = [ysize; xsize]
+smld_true, smld_model, smld = SMLMSim.sim(;
+    ρ=0.1, # emitters / pixel^2
+    σ_PSF=1.3, # pixels
+    minphotons=50,
+    ndatasets=10,
+    nframes=1000,
+    framerate=1.0, # set to 1.0 to keep things in camera units
+    pattern=SMLMSim.Nmer2D(; n=8, d=1.0),
+    molecule=SMLMSim.GenericFluor(; q=[0 0.5; 1e-3 0]), # 1 / frame 
+    camera=SMLMSim.IdealCamera(; xpixels=16, ypixels=16, pixelsize=1.0) # set pixelsize to 1.0 to keep things in camera units
+)
 
 # Perform BaGoL.
 params = SMLMBaGoL.BaGoLParams2D()
@@ -44,38 +31,35 @@ params.prethresholds.r = 10.0
 params.preclustering.on = true
 params.preclustering.maxdist = 0.15
 params.mcparams.σ_a = 0.0
-params.mcparams.η = 5.0
-params.mcparams.γ = 10.0
-# params.mcparams.η = 2.0
-# params.mcparams.γ = (length(smld_model) / length(smld_true)) * params.mcparams.η
+# params.mcparams.η = 5.0
+# params.mcparams.γ = 10.0
+params.mcparams.η = 2.0
+params.mcparams.γ = (length(smld_model) / length(smld_true)) / params.mcparams.η
 params.mcparams.srmag = 10.0
 params.mcparams.nsigma = 5.0
 params.mcparams.p_jump = [1.0; 1.0; 1.0; 1.0]
 params.mcparams.p_jump = params.mcparams.p_jump / sum(params.mcparams.p_jump)
 params.mcparams.n_burnin = 2000
 params.mcparams.n_chain = 3000
-# chain, rois, roioverlap = SMLMBaGoL.runbagol(smld; 
-#     subregion_params = params.subregion, 
-#     prethresholds = params.prethresholds, 
-#     preclustering_params = params.preclustering, 
-#     mcparams = params.mcparams);
-chain, λchain, rois, roioverlap = SMLMBaGoL.runbagol(smld, params.hbparams;
-    subregion_params = params.subregion,
-    prethresholds = params.prethresholds,
-    preclustering_params = params.preclustering,
+chain, validchain, mapnout, rois, roioverlap = SMLMBaGoL.runbagol(smld; 
+    subregion_params = params.subregion, 
+    prethresholds = params.prethresholds, 
+    preclustering_params = params.preclustering, 
     mcparams = params.mcparams);
-validchain = SMLMBaGoL.removeoverlap(chain, smld.datasize, rois, roioverlap)
-mapnout = SMLMBaGoL.mapn(validchain)
+# chain, validchain, mapnout, λchain, rois, roioverlap = SMLMBaGoL.runbagol(smld, params.hbparams;
+#     subregion_params=params.subregion,
+#     prethresholds=params.prethresholds,
+#     preclustering_params=params.preclustering,
+#     mcparams=params.mcparams);
 
 # Plot some results.
-Plots.histogram(mean.(mapnout[5]))
+Plots.histogram(mean.(mapnout[5])) # localizations per emitter
 Plots.histogram(mapnout[6])
 srmag = 100.0
-gaussim = SMLMData.makegaussim(mapnout[1], mapnout[2], smld.datasize)
-circleim_mapn = SMLMData.makecircleim(mapnout[1], vec(mean(mapnout[2], dims = 2)), smld.datasize, srmag)
+circleim_mapn = SMLMData.makecircleim(mapnout[1], vec(mean(mapnout[2], dims=2)), smld.datasize, srmag)
 coords = [smld.y smld.x]
 σ_coords = [smld.σ_y smld.σ_x]
-circleim = SMLMData.makecircleim(coords, vec(mean(σ_coords, dims = 2)), smld.datasize, srmag)
+circleim = SMLMData.makecircleim(coords, vec(mean(σ_coords, dims=2)), smld.datasize, srmag)
 circleim ./= maximum(circleim)
 circleim_mapn ./= maximum(circleim_mapn)
 testim = RGB.(circleim, circleim_mapn, circleim)
